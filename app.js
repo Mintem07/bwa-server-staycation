523c7647c2d4780bba09e772c5fa4c8205ffaa7b
Mintem07 <Mintem07@github.com>
2020-12-15 05:26:42 +0700

    add install cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors');
 
 // import mongoose
 const mongoose = require('mongoose');
@@ -35,6 +36,7 @@ app.use(session({
   cookie: { maxAge: 60000 }
 }));
 app.use(flash());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
